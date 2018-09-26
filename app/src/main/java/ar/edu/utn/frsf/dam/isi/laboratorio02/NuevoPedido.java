a48da23f2e36b6d15b0842cc8c3aed43fd7b764c
Martin <mar.inc.987@gmail.com>
2018-09-26 02:03:09 -0300

    Actividad hasta 3.

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedido.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedido.java
@@ -2,14 +2,46 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.PedidoDetalle;
 
 public class NuevoPedido extends AppCompatActivity {
+    private RadioButton rBtnRetirEnLocal;
+    private RadioButton rBtnEntregaDomicilio;
+    private EditText edtDireccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuevo_pedido);
 
+
+
+        ArrayAdapter<PedidoDetalle>  adapDetalle = new ArrayAdapter<PedidoDetalle>(this, android.R.layout.simple_list_item_1,(ArrayList<PedidoDetalle>) this.getIntent().getSerializableExtra("Detalles"));
+        rBtnEntregaDomicilio= (RadioButton)  findViewById(R.id.radioButtonEntregaDomicilio);
+        rBtnRetirEnLocal= (RadioButton)  findViewById(R.id.radioButtonRetirEnLocal);
+        edtDireccion= (EditText) findViewById(R.id.editTextDireccion);
+
+        rBtnEntregaDomicilio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtDireccion.setEnabled(true);
+            }
+        });
+        rBtnRetirEnLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtDireccion.setEnabled(false);
+            }
+        });
 
     }
 }
