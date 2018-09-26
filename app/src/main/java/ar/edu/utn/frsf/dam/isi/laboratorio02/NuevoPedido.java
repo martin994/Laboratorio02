69844b46577c33bfecff14cb140bb3b1ce8704b1
Martin <mar.inc.987@gmail.com>
2018-09-26 04:51:39 -0300

    Actividad hasta 3, hasta el i-III.

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedido.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/NuevoPedido.java
@@ -10,7 +10,10 @@ import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +28,15 @@ public class NuevoPedido extends AppCompatActivity {
     private EditText edtDireccion;
     private ListView lVPedido;
     private Button btnPedidoAddProducto;
+    private TextView tVTotalPedido;
+    private Button btnHacerPedido;
+    private EditText edtCorreo;
+    private EditText edtHoraEntrega;
+
     private Producto nuevoProducto=null;
     private PedidoDetalle nuevoDetalle;
+    private Pedido nuevoPedido;
+
     @Override
     protected void onActivityResult(int requestCode,int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);//sobreescritura para respues de los intents
@@ -35,6 +45,9 @@ public class NuevoPedido extends AppCompatActivity {
         }else{
             nuevoProducto =(Producto) data.getSerializableExtra("Producto");
             nuevoDetalle=new PedidoDetalle(data.getIntExtra("Cantidad",-1), nuevoProducto);
+            nuevoDetalle.setPedido(nuevoPedido);
+            tVTotalPedido.setText(R.string.tvTotal + nuevoPedido.total().toString());
+
         }
 
     }
@@ -45,7 +58,7 @@ public class NuevoPedido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nuevo_pedido);
-
+        nuevoPedido=new Pedido();
 
         //ArrayAdapter<PedidoDetalle> adapDetalle = new ArrayAdapter<PedidoDetalle>(this, android.R.layout.simple_list_item_1, (ArrayList<PedidoDetalle>) this.getIntent().getSerializableExtra("Detalles"));
         ArrayAdapter<PedidoDetalle> adapDetalle = new ArrayAdapter<PedidoDetalle>(this, android.R.layout.simple_list_item_1, new ArrayList<PedidoDetalle>());
@@ -56,7 +69,8 @@ public class NuevoPedido extends AppCompatActivity {
         lVPedido= (ListView) findViewById(R.id.listViewPedidoItems);
         lVPedido.setAdapter(adapDetalle);
         btnPedidoAddProducto=(Button) findViewById(R.id.buttonAgregar);
-
+        tVTotalPedido = (TextView) findViewById(R.id.textViewTotalPedido);
+        btnHacerPedido = (Button) findViewById(R.id.buttonPedidoRealizar);
         rBtnEntregaDomicilio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,5 +94,20 @@ public class NuevoPedido extends AppCompatActivity {
 
             }
         });
+        btnHacerPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtDireccion.getText().length()==0 || edtHoraEntrega.getText().length()==0 || edtCorreo.getText().length()==0){
+                    CharSequence text = "Campos invalidos!";
+
+                    Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+        });
+        nuevoPedido.setEstado(Pedido.Estado.REALIZADO);
+        Intent i = new Intent(NuevoPedido.this, MainActivity.class);
+        i.putExtra("Pedido", (Serializable) nuevoPedido);//Implementar la clase serializable en los DAO y modelos
     }
 }
