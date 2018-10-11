228bf944abffd311eebd5e4ede7620c292c2bcd7
Martin <mar.inc.987@gmail.com>
2018-10-11 19:15:58 -0300

    Lab03-Req04-Actividad05, funcionalidad a medio terminar, no se como enviar mensajes desde firebase para comprobar su funcionalidad

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/RestoFirebaseInstanceIdService.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/RestoFirebaseInstanceIdService.java
@@ -2,7 +2,9 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
@@ -13,5 +15,15 @@ public class RestoFirebaseInstanceIdService extends FirebaseInstanceIdService {
     public void onTokenRefresh(){
 
     }
-
+    private void guardarToken(String _token){
+        SharedPreferences preferences =PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("registration_id", _token);
+        editor.apply();
+    }
+    private
+    String leerToken(){
+        SharedPreferences preferences =PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getString("registration_id",null);
+    }
 }
---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/RestoMessagingService.java
@@ -0,0 +1,31 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+
+public class RestoMessagingService extends FirebaseMessagingService {
+    PedidoRepository repoPedido = null;
+    Pedido actual= null;
+    public RestoMessagingService() {
+    }
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        repoPedido=new PedidoRepository();
+
+        actual=repoPedido.buscarPorId(Integer.getInteger(remoteMessage.getData().get("ID_PEDIDO")));
+        actual.setEstado(Pedido.Estado.LISTO);
+        Intent broad = new Intent();
+        broad.putExtra("id_pedido", actual.getId());
+        broad.setAction(EstadoPedidoReciver.Evento04);
+        sendBroadcast(broad);
+
+    }
+
+}
