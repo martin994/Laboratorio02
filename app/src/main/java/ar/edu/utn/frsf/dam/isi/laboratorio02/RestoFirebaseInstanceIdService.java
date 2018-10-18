dff801116c811140310d4f74912064758b3c37cd
Martin <mar.inc.987@gmail.com>
2018-10-18 20:51:39 -0300

    Lab03-Req04-Actividad05, funcionalidad a medio terminar, recepcion de notificacion implementada, sin pruebas

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/RestoFirebaseInstanceIdService.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/RestoFirebaseInstanceIdService.java
@@ -11,7 +11,7 @@ import android.util.Log;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class RestoFirebaseInstanceIdService extends FirebaseInstanceIdService {
+public class RestoFirebaseInstanceIdService {/*extends FirebaseInstanceIdService {
     public static final String TAG = "RestoFirebaseInstanceIdService";
     public RestoFirebaseInstanceIdService() {
     }
